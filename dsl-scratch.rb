34cdeee00de21f75b26f3ba7073e8a526bf8bd28
Alex Dean <alexander.dean@keplarllp.com>
2013-08-31 14:31:46 +0300

    Updated

--- dsl-scratch.rb
+++ dsl-scratch.rb
@@ -10,9 +10,9 @@ api_user = Snowplow::Subject.new(business_user_id='gpfeed')
 ctx = Snowplow::Context.new('web', app_id='shop')
 
 # Track some events
-tracker.track ctx, do
+tracker.track do
   end_user views web_page
   end_user places sales_order, ~: ctx.on(web_page)
   end_user performs struct_event, ~: ctx.on(web_page).at(event_tstamp)
   api_user performs unstruct_event, ~: Snowplow::Context.new('pc')
-end
+end, ~: ctx
