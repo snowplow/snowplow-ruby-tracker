63b4855672d16bbb84ea19bdcc34bf750f7e86e2
Alex Dean <alexander.dean@keplarllp.com>
2013-08-31 14:41:23 +0300

    Stubbed event Object defns

--- dsl-scratch.rb
+++ dsl-scratch.rb
@@ -5,7 +5,7 @@ tracker = Snowplow::Tracker.new(collector)
 
 # Create a couple of Subjects
 end_user = Snowplow::Subject.new(ip_address='x.x.x.x', business_user_id='runkelfinker')
-api_user = Snowplow::Subject.new(business_user_id=hostname)
+api_user = Snowplow::Subject.new(business_user_id=Socket.gethostname)
 
 # Setup a couple of Contexts
 end_ctx = Snowplow::Context.new('web', app_id='shop')
@@ -13,6 +13,12 @@ api_ctx = Snowplow::Context.new('pc', app_id='api')
 
 # We can pin a Context to our Tracker
 tracker.pin end_ctx
+
+# Create our events' Objects
+web_page = WebPage.new(...) # We will use this for Context too
+sales_order = SalesOrder.new(...)
+struct_event = StructEvent.new(...)
+unstruct_event = UnstructEvent.new(...)
 
 # Track some events
 tracker.track do
