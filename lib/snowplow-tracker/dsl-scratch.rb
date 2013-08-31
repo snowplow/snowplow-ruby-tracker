fec6a6b3c4d2d93fb845ae2cabc9f816b9d21702
Alex Dean <alexander.dean@keplarllp.com>
2013-08-31 13:02:42 +0300

    Updated

--- lib/snowplow-tracker/dsl-scratch.rb
+++ lib/snowplow-tracker/dsl-scratch.rb
@@ -1,9 +1,8 @@
 tracker = SnowplowTracker.new
 
-# returns our aggregated data
-tracker.track(ctx) do
-  end_user views web_page
-  end_user places sales_order
-  end_user performs struct_event
+# Event DSL
+tracker.track do
+  end_user views web_page :~ ctx
+  end_user places sales_order :~ ctx.on(web_page)
+  end_user performs struct_event :~ ctx.on(web_page).at(event_tstamp)
 end
-
