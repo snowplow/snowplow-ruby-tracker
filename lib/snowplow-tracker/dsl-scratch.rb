a86fbb52b4ae39b2d09027563b498535f89e0b75
Alex Dean <alexander.dean@keplarllp.com>
2013-08-31 13:07:33 +0300

    Fixed syntax

--- lib/snowplow-tracker/dsl-scratch.rb
+++ lib/snowplow-tracker/dsl-scratch.rb
@@ -2,7 +2,7 @@ tracker = SnowplowTracker.new
 
 # Event DSL
 tracker.track do
-  end_user views web_page :~ ctx
-  end_user places sales_order :~ ctx.on(web_page)
-  end_user performs struct_event :~ ctx.on(web_page).at(event_tstamp)
+  end_user views web_page ~: ctx
+  end_user places sales_order ~: ctx.on(web_page)
+  end_user performs struct_event ~: ctx.on(web_page).at(event_tstamp)
 end
