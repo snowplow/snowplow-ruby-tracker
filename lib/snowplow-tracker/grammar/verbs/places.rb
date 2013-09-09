68b3275ef385ef5b5c63b3c2e13ae2c7e449d5f3
Alex Dean <alexander.dean@keplarllp.com>
2013-09-09 23:53:44 +0100

    Added missing files

---
+++ lib/snowplow-tracker/grammar/verbs/places.rb
@@ -0,0 +1,73 @@
+# Copyright (c) 2013 Snowplow Analytics Ltd. All rights reserved.
+#
+# This program is licensed to you under the Apache License Version 2.0,
+# and you may not use this file except in compliance with the Apache License Version 2.0.
+# You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Apache License Version 2.0 is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+
+# Author::    Alex Dean (mailto:snowplow-user@googlegroups.com)
+# Copyright:: Copyright (c) 2013 Snowplow Analytics Ltd
+# License::   Apache License Version 2.0
+
+require 'contracts'
+include Contracts
+
+module Snowplow
+
+  module Places
+
+    include Protocol
+
+    # Defines the valid Transaction Symbols
+    class TransactionSymbol
+
+      @@valid_transactions = Set::[](:tr, :ti)
+
+      def self.valid?(val)
+        val.is_a? Symbol &&
+          @@valid_transactions.include?(val)
+      end
+    end
+
+    # Converts this Verb into a Hash representing its
+    # event type, ready for adding to the payload.
+    # Follows the Snowplow Tracker Protocol:
+    #
+    # https://github.com/snowplow/snowplow/wiki/snowplow-tracker-protocol
+    #
+    # Parameters:
+    # +tr_symbol+:: whether the Verb is referring to placing
+    #               a transaction, or recording a transaction
+    #               item within the transaction. Nasty but
+    #               needed to handle Snowplow's Tracker
+    #               Protocol (which needs to change).
+    #
+    # Returns a VerbHash 
+    Contract TransactionSymbol => VerbHash
+    def as_hash(tr_symbol)
+      to_protocol([ 'e', tr_symbol.to_s, :raw ])
+    end
+    module_function :as_hash
+
+    # Subject places an ecommerce transaction.
+    #
+    # Parameters:
+    # +sales_order+:: the sales order to track,
+    #                 including order line items
+    # +context+:: the optional Context in which this event
+    #             takes place. Overrides any pinned Context
+    Contract SalesOrder, OptionContext => nil # TODO: fix return
+    def places(sales_order,
+               context=nil)                 
+
+      nil # TODO: fix return
+    end
+    module_function :places
+
+  end
+
+end
---
+++ lib/snowplow-tracker/grammar/verbs/views.rb
@@ -0,0 +1,63 @@
+# Copyright (c) 2013 Snowplow Analytics Ltd. All rights reserved.
+#
+# This program is licensed to you under the Apache License Version 2.0,
+# and you may not use this file except in compliance with the Apache License Version 2.0.
+# You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Apache License Version 2.0 is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+
+# Author::    Alex Dean (mailto:snowplow-user@googlegroups.com)
+# Copyright:: Copyright (c) 2013 Snowplow Analytics Ltd
+# License::   Apache License Version 2.0
+
+require 'contracts'
+include Contracts
+
+module Snowplow
+
+  module Views
+
+    include Protocol
+
+    # Converts this Verb into a Hash representing its
+    # event type, ready for adding to the payload.
+    # Follows the Snowplow Tracker Protocol:
+    #
+    # https://github.com/snowplow/snowplow/wiki/snowplow-tracker-protocol
+    #
+    # Returns a VerbHash 
+    Contract => VerbHash
+    def as_hash()
+      to_protocol([ 'e', 'pv', :raw ])
+    end
+    module_function :as_hash
+
+    # Subject views a web page.
+    #
+    # WARNING: all the Web's tiers of caching mean
+    # that relying on your web server to track
+    # page views is almost always a BAD IDEA.
+    #
+    # Use the Snowplow JavaScript Tracker instead:
+    # https://github.com/snowplow/snowplow-javascript-tracker
+    #
+    # Parameters:
+    # +web_page+:: the WebPage the user is viewing
+    # +context+:: the optional Context in which this event
+    #             takes place. Overrides any pinned Context
+    #
+    # Returns ??
+    Contract WebPage, OptionContext => nil # TODO: fix return
+    def views(web_page,
+              context=nil)
+
+      nil # TODO: fix return
+    end
+    module_function :views
+
+  end
+
+end
