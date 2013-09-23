21be971dfab215a0469c747e11179bffdacbba07
Alex Dean <alexander.dean@keplarllp.com>
2013-09-23 10:11:42 +0300

    Updated

--- dsl-scratch.rb
+++ dsl-scratch.rb
@@ -3,7 +3,7 @@
 tracker {
   collector :main, cf: 'd1vjp94kduqgnd'
   subject :end_user, ip_address: 'x.x.x.x', business_user_id: 'runkelfinker'
-  context :end_ctx, platform: :web, app_id: 'shop'
+  default_context :end_ctx, platform: :web, app_id: 'shop'
   subject :api_user, business_user_id: Socket.gethostname
   context :api_ctx, platform: :pc, app_id: 'api'
 }
@@ -16,8 +16,8 @@ unstruct_event = Snowplow::UnstructEvent.new(...)
 
 # Track some events
 track {
-  end_user views web_page, ~: end_ctx
-  nd_user places sales_order, ~: end_ctx.on(web_page)
+  end_user views web_page # Uses default_context
+  end_user places sales_order, ~: end_ctx.on(web_page)
   end_user performs struct_event, ~: end_ctx.on(web_page).at(event_tstamp)
   api_user performs unstruct_event, ~: api_ctx
 }
