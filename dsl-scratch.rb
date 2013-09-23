0ea6db226eb112555c4b6dae05ee70ecd11b64c8
Alex Dean <alexander.dean@keplarllp.com>
2013-09-23 10:12:20 +0300

    Updated

--- dsl-scratch.rb
+++ dsl-scratch.rb
@@ -2,8 +2,10 @@
 # Define our Tracker
 tracker {
   collector :main, cf: 'd1vjp94kduqgnd'
+  
   subject :end_user, ip_address: 'x.x.x.x', business_user_id: 'runkelfinker'
   default_context :end_ctx, platform: :web, app_id: 'shop'
+  
   subject :api_user, business_user_id: Socket.gethostname
   context :api_ctx, platform: :pc, app_id: 'api'
 }
@@ -19,5 +21,6 @@ track {
   end_user views web_page # Uses default_context
   end_user places sales_order, ~: end_ctx.on(web_page)
   end_user performs struct_event, ~: end_ctx.on(web_page).at(event_tstamp)
+  
   api_user performs unstruct_event, ~: api_ctx
 }
