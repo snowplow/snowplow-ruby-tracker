3cbccf2623e7ce992ef431dc3c81fba766b9e147
Ed Lewis <edward.richard.lewis@gmail.com>
2016-08-12 13:36:59 +0100

    Add support for attaching true timestamp to events (closes #94)

---
+++ spec/unit/timestamp_spec.rb
@@ -0,0 +1,53 @@
+# Copyright (c) 2016 Snowplow Analytics Ltd. All rights reserved.
+#
+# This program is licensed to you under the Apache License Version 2.0,
+# and you may not use this file except in compliance with the Apache License Version 2.0.
+# You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Apache License Version 2.0 is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+
+# Author:: Alex Dean, Fred Blundun, Ed Lewis (mailto:support@snowplowanalytics.com)
+# Copyright:: Copyright (c) 2016 Snowplow Analytics Ltd
+# License:: Apache License Version 2.0
+
+require 'spec_helper'
+
+describe SnowplowTracker::Timestamp do
+
+    it 'has a type' do
+        SnowplowTracker::Timestamp.new('dtm', 123456).type.should eq("dtm")
+    end
+
+    it 'has a value' do 
+        SnowplowTracker::Timestamp.new('dtm', 123456).value.should eq(123456)
+    end
+
+end
+
+describe SnowplowTracker::TrueTimestamp do 
+
+    it 'has type ttm' do 
+        SnowplowTracker::TrueTimestamp.new(1234).type.should eq('ttm')
+    end
+
+    it 'has a value' do 
+        SnowplowTracker::TrueTimestamp.new(1234).value.should eq(1234)
+    end
+
+end
+
+describe SnowplowTracker::DeviceTimestamp do 
+
+    it 'has type dtm' do 
+        SnowplowTracker::DeviceTimestamp.new(1234).type.should eq('dtm')
+    end
+
+    it 'has a value' do 
+        SnowplowTracker::DeviceTimestamp.new(1234).value.should eq(1234)
+    end
+
+end
+
